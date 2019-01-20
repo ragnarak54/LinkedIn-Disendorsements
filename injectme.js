dbbc282ddd2d2e919b3dc9335820451112477fbd
Jfleit <jfleit18@gmail.com>
2019-01-19 20:07:18 -0700

    trying to get to div and insert after

--- injectme.js
+++ injectme.js
@@ -1,2 +1,8 @@
-profile_div = document.getElementsByClassName("profile-detail");
+var disendorsement_div = document.createElement("div");
+disendorsement_div.style.background = "pink";
+disendorsement_div.innerHTML = "This guy sucks!";
+
+var profile_div = document.getElementsByClassName("profile-detail")[0];
 console.log(profile_div);
+console.log(profile_div[5]);
+//profile_div[5].parentNode.insertBefore(disendorsement_div, profile_div[5].nextSibling);
